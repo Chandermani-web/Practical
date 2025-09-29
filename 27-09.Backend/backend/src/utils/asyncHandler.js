61e056c3006de9fc139588fa9fe8344a643ffce4
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-09-29 17:59:24 +0530

    creating a asynchandler for reduce the repitition of the try-catch block

--- 27-09.Backend/backend/src/utils/asyncHandler.js
+++ 27-09.Backend/backend/src/utils/asyncHandler.js
@@ -1,6 +1,3 @@
 export const asyncHandler = (fn) => (req,res,next) => {
-    return Promise.resolve(fn(req,res,next)).catch((error) => {
-        res.status(500).json({ message: "Something went wrong" });
-        next(error);
-    });
+    return Promise.resolve(fn(req,res,next)).catch(next);
 }
