6a847f6dbb517796c1ac2a8f858432332b44e579
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-09-29 17:58:31 +0530

    auth routes for handling the registration, login and logout and upload and update profile

--- 27-09.Backend/backend/src/routes/auth.route.js
+++ 27-09.Backend/backend/src/routes/auth.route.js
@@ -1,16 +1,22 @@
 import express from 'express';
-import { GetProfile, UpdateProfile } from '../controllers/profile.controller.js';
+import { GetProfile, UpdateProfile, UploadProfilePic, UploadCoverPic } from '../controllers/profile.controller.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 import { login, logout, signup } from '../controllers/auth.controller.js';
+import upload from '../middlewares/upload.js';
+
 const router = express.Router();
 
 // registration, login, logout 
-router.post("/signup",signup);
-router.post("/login",login);
-router.post("/logout",logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 // profile update and get
 router.put("/updateprofile", isLoggedIn, UpdateProfile);
-router.get("/profile", isLoggedIn, GetProfile);
+router.get("/me", isLoggedIn, GetProfile);
+
+// photo upload routes
+router.post("/upload-profile-pic", isLoggedIn, upload.single('profilePic'), UploadProfilePic);
+router.post("/upload-cover-pic", isLoggedIn, upload.single('coverPic'), UploadCoverPic);
 
 export default router;
