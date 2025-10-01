6eea53b737100e2e9ec0e2e22b81f3511df5acfd
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-01 19:59:13 +0530

    Create the routes for the naviagation

--- 27-09.Backend/frontend/src/App.jsx
+++ 27-09.Backend/frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useContext } from "react";
+import { useContext, useEffect } from "react";
 import AppContext from "./Context/UseContext";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import CreatePost from "./pages/CreatePost";
+import CreatePost from "./Components/Post/CreatePost";
 import Navbar from "./Components/Common/Navbar";
+import Connection from "./pages/Connection";
 
 const App = () => {
-  const { fetchUser, auth, fetchPosts } = useContext(AppContext);
+  const { auth } = useContext(AppContext);
 
-  useEffect(() => {
-    fetchUser();
-    fetchPosts();
-  },[]);
+  useEffect(()=>{
+    console.log("Auth state changed:", auth);
+  })
 
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <div className="bg-gray-900 min-h-screen text-gray-50">
       <Navbar />
 
       <Routes>
@@ -26,6 +26,7 @@ const App = () => {
         <Route path="/signup" element={auth ? <Home /> : <Signup />} />
         <Route path="/login" element={auth ? <Home /> : <Login />} />
         <Route path="/create-post" element={auth ? <CreatePost /> : <Login />} />
+        <Route path="/connections" element={auth ? <Connection /> : <Login />} />
       </Routes>
     </div>
   );
