63d4e2d389a9aee79a040d618173d8d1fd52d0bf
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-02 13:56:05 +0530

    created a friend routes for using the friends related api and also operation with friends collection

---
+++ 27-09.Backend/backend/src/routes/friend.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { isLoggedIn } from '../middlewares/isLoggedIn.js';
+import { sendFriendRequest, acceptFriendRequest, rejectFriendRequest, removeFriend, listFriends, listFriendRequests } from '../controllers/friendrequest.controller.js';
+
+const router = express.Router();
+
+router.post("/send-request", isLoggedIn, sendFriendRequest);
+router.post("/accept-request", isLoggedIn, acceptFriendRequest);
+router.post("/reject-request", isLoggedIn, rejectFriendRequest);
+router.post("/remove-friend", isLoggedIn, removeFriend);
+router.get("/list", isLoggedIn, listFriends);
+router.get("/requests", isLoggedIn, listFriendRequests);
+
+export default router;
