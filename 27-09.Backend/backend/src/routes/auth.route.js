72ff0258bfd4ad8f96e1849d50425d1c54804da0
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-02 13:56:50 +0530

    doing adding somethings in the auth route

--- 27-09.Backend/backend/src/routes/auth.route.js
+++ 27-09.Backend/backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { GetProfile, UpdateProfile, UploadProfilePic, UploadCoverPic } from '../controllers/profile.controller.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, AllUser } from '../controllers/auth.controller.js';
 import upload from '../middlewares/upload.js';
 
 const router = express.Router();
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/AllUser", isLoggedIn, AllUser);
 
 // profile update and get
 router.put("/updateprofile", isLoggedIn, UpdateProfile);
@@ -18,5 +19,6 @@ router.get("/me", isLoggedIn, GetProfile);
 // photo upload routes
 router.post("/upload-profile-pic", isLoggedIn, upload.single('profilePic'), UploadProfilePic);
 router.post("/upload-cover-pic", isLoggedIn, upload.single('coverPic'), UploadCoverPic);
+
 
 export default router;
