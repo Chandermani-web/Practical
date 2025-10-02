a6b902378f3cbf0edce37891dfd5e38491398fce
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-02 13:59:24 +0530

    now connect with cloudinary

--- 27-09.Backend/backend/src/middlewares/upload.js
+++ 27-09.Backend/backend/src/middlewares/upload.js
@@ -1,21 +1,28 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../utils/cloudinary.js";
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../utils/cloudinary.js';
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
-    let folder = "social-app"; // default folder
-    if (file.mimetype.startsWith("image")) folder = "social-app/images";
-    else if (file.mimetype.startsWith("video")) folder = "social-app/videos";
+    let folder = 'posts';
+    let resource_type = 'auto'; // auto-detects image/video
+
+    if (file.mimetype.startsWith('video')) {
+      resource_type = 'video';
+      folder += '/videos';
+    } else if (file.mimetype.startsWith('image')) {
+      resource_type = 'image';
+      folder += '/images';
+    }
 
     return {
       folder,
-      allowed_formats: ["jpg", "jpeg", "png", "gif", "mp4", "mov", "webm"],
+      resource_type,
     };
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage , limits: { fileSize: 50 * 1024 * 1024 } }); // 50MB limit
 
 export default upload;
