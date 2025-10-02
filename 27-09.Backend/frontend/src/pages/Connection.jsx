95b5e6eb4c4499b072e4e0f56fb415ab6f93ace9
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-02 14:02:16 +0530

    Connection page

--- 27-09.Backend/frontend/src/pages/Connection.jsx
+++ 27-09.Backend/frontend/src/pages/Connection.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import { useState } from 'react';
 import Sidebar from '../Components/Connections/Sidebar'
-import AllUser from '../Components/Connections/AllUser'
+import ShowAllUser from '../Components/Connections/Page/ShowAllUser';
+import YourTotalConnection from '../Components/Connections/Page/YourTotalConnection';
+import SendRequestConnection from '../Components/Connections/Page/SendRequestConnection';
+import ReceiveRequestConnection from '../Components/Connections/Page/ReceiveRequestConnection';
+import Blockuser from '../Components/Connections/Page/Blockuser';
 
 const Connection = () => {
+  const [displayName, setdisplayName] = useState(1);
+
+  const values = [
+    { id: 1, element: <ShowAllUser />  },
+    { id: 2, element: <YourTotalConnection /> },
+    { id: 3, element: <SendRequestConnection /> },
+    { id: 4, element: <ReceiveRequestConnection /> },
+    { id: 5, element: <Blockuser /> },
+  ]
+
   return (
     <div className='grid lg:grid-cols-4 sm:grid-cols-1 gap-4'>
       <div className='mt-5'>
-        <Sidebar />
+        <Sidebar setdisplayName={setdisplayName} />
       </div>
 
       <div className='lg:col-span-3 mt-5 mx-4'>
-        <h1 className='text-white text-2xl font-semibold ml-5'>Suggestion For You</h1>
-      <AllUser />
+        
+        { displayName === 1 ? (
+            <ShowAllUser />
+          ) : displayName === 2 ? (
+            <YourTotalConnection />
+          ) : displayName === 3 ? (
+            <SendRequestConnection />
+          ) : displayName === 4 ? (
+            <ReceiveRequestConnection />
+          ) : displayName === 5 ? (
+            <Blockuser />
+          ) : null
+        }
+
       </div>
     </div>
   )
