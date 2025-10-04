ec25cb4155519c13e2b981a7e40f30bf02118d45
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-04 23:33:50 +0530

    routes for the notification controllers

---
+++ 27-09.Backend/backend/src/models/Notification.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // user who receives
+  type: { 
+    type: String, 
+    enum: ["friend_request", "like", "post", "message"], 
+    required: true 
+  },
+  fromUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // who triggered
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post" }, // related post if any
+  message: { type: String },
+  isRead: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
